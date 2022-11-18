ef45133b0ea4cabd96f8c0016ac0e719a9497b1d
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-11-18 11:44:30 +0530

    useMemoized and useFuture imlmented in flutter hooks

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?>{
+  Iterable<T> compactMap<E>([E? Function(T?)? transform]) => map(transform ?? (e) => e).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +13,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,17 +25,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+const url = "https://www.studyinserbia.rs/uploads/attachment/strana/228/large_tara-mountain-5520592_1920.jpg";
+
+class MyHomePage extends HookWidget {
+
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    /// grab the image from url
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url)).load(url).then((data) => data.buffer.asUint8List()).then((data) => Image.memory(data)));
+    /// useFuture is a hook widget and we will use if we have to return Future function
+    /// useMemoized allows caching of complex objects
+    final snapshot = useFuture(future);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HomePage"),
+      ),
+      body: Column(
+        children: [
+          snapshot.data
+        ].compactMap().toList(),
+      ),
+    );
   }
 }
-
