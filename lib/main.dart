e509370b958ddb3e021a3ada0e8b147ab332c01d
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-11-21 11:03:21 +0530

    hook useAnimationController added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +21,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url = "https://bit.ly/3x7J5Qt";
+const imageHeight = 300.0;
+
+/// It is a compile-time error for any type other than [int] or [double] to attempt to extend or implement `num`.
+extension Normalize on num{
+num normalized(
+    num selfRangeMin,
+    num selfRangeMax,[
+      num normalizedRangeMin = 0.0,
+      num normalizedRangeMax = 1.0,
+
+]
+    ) => (normalizedRangeMax - normalizedRangeMin) * ((this - selfRangeMin) / (selfRangeMax- selfRangeMin)) + normalizedRangeMin;
+}
+
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -30,7 +47,14 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final opacity = useAnimationController(duration: const Duration(seconds: 1),initialValue: 1.0,lowerBound: 0.0,upperBound: 1.0,);
+    final size = useAnimationController(duration: const Duration(seconds: 1),initialValue: 1.0,lowerBound: 0.0,upperBound: 1.0,);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+    );
   }
 }
 
