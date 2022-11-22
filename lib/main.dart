adc1f99ee1bde7eba3f6744974e76c746bb774ff
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-11-22 15:31:15 +0530

    useStreamController, StreamBuilder, RotationTransition

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,17 +22,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+const url = "https://bit.ly/3x7J5Qt";
+
+class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    late final StreamController<double> streamController;
+    streamController = useStreamController<double>(onListen: () {
+      streamController.sink.add(0.0);
+    });
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Stream Controller"),
+      ),
+      /// Creates a rotation transition.
+      body: StreamBuilder<double>(
+          stream: streamController.stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final rotation = snapshot.data ?? 0.0;
+              return GestureDetector(
+                onTap: () {
+                  streamController.sink.add(rotation + 10.0);
+                },
+                child: RotationTransition(
+                    /// An animation that is always stopped at a given value. The [status] is always [AnimationStatus.forward].
+                    turns: AlwaysStoppedAnimation(rotation / 360),
+                    child: Center(child: Image.network(url))),
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
+          }),
+    );
   }
 }
-
