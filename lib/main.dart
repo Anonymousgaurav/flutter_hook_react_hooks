d2b1c2a3efe902557550a90e36b21e5a199226be
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-11-25 12:06:51 +0530

    useAppLifecycleState

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,17 +21,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    /// Returns the current [AppLifecycleState] value and rebuilds the widget when it changes.
+    final lifeCycle = useAppLifecycleState();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Home Page"),
+      ),
+      body: Opacity(
+        opacity: lifeCycle == AppLifecycleState.resumed ? 1.0 : 0.0,
+        child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 10.0,
+                  color: Colors.black.withAlpha(100),
+                  spreadRadius: 10.0
+                )
+              ]
+            ),child: Image.asset("assets/credit_card.png"),
+        ),
+      ),
+    );
   }
 }
 
